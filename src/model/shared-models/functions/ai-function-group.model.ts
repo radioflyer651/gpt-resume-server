f1f8c3cf26494ab53c6fd6a0008da176a6e7c51c
Richard Olson <richard@richardolson.org>
2025-03-28 15:37:36 -0500

    Add groupName property to AiFunctionGroup interface for better function categorization

--- src/model/shared-models/functions/ai-function-group.model.ts
+++ src/model/shared-models/functions/ai-function-group.model.ts
@@ -4,6 +4,9 @@ import * as x from 'openai/resources/responses/input-items';
 /** A group of AiFunctions that work as a group or category. */
 export interface AiFunctionGroup {
 
+    /** Provides a name for this group of functions. */
+    groupName: string;
+
     /** Gets or sets a list of functions that belong to this group. */
     functions: AiFunctionDefinition[];
 }
