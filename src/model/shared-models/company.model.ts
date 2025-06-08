1ca7ed8c7eb6254f6dde7e2142e371444a41d99b
Richard Olson <richard@richardolson.org>
2025-06-07 19:53:21 -0500

    Add optional jobsSite field to Company interface for job board URL

--- src/model/shared-models/company.model.ts
+++ src/model/shared-models/company.model.ts
@@ -6,11 +6,14 @@ export interface Company {
     name: string;
     website: string;
 
+    /** If available, the URL to their job board. */
+    jobsSite?: string;
+
     /** Boolean value indicating whether or not this company should be archived (and hidden from view). */
     archive?: boolean;
 
     comments?: Comment[];
-    
+
     /** Optional: The ID of the ApolloCompany, if one exists, in the database. */
     apolloId?: ObjectId;
 }
